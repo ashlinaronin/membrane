304df58b06959ec4bcc6114b7e40e85a33f452c8
ashlinaronin <ashlinaronin@gmail.com>
2018-08-24 14:00:01 -0500

    Track fullscreen state locally

--- src/components/FullScreenButton.vue
+++ src/components/FullScreenButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="full-screen-button">
-    <button @click="doFscreen">full screen</button>
+    <button @click="toggleFullscreenMode">{{ buttonText }}</button>
   </div>
 </template>
 
@@ -9,27 +9,51 @@ import fscreen from "fscreen";
 
 export default {
   name: "FullScreenButton",
+  data() {
+    return {
+      fullscreen: false
+    };
+  },
+  computed: {
+    buttonText() {
+      return this.fullscreen ? "Exit full screen" : "Enter full screen";
+    }
+  },
+  mounted() {
+    if (fscreen.fullscreenEnabled) {
+      fscreen.addEventListener(
+        "fullscreenchange",
+        this.handleFullscreenChange,
+        false
+      );
+      fscreen.addEventListener(
+        "fullscreenerror",
+        this.handleFullscreenError,
+        false
+      );
+    }
+  },
   methods: {
-    doFscreen() {
-      if (fscreen.fullscreenEnabled) {
-        fscreen.addEventListener(
-          "fullscreenchange",
-          this.fullscreenHandler,
-          false
-        );
+    toggleFullscreenMode() {
+      if (fscreen.fullscreenElement === null) {
         fscreen.requestFullscreen(document.body);
+      } else {
+        fscreen.exitFullscreen();
       }
     },
-    fullscreenHandler() {
-      if (fscreen.fullscreenElement !== null) {
-        console.log("Entered fullscreen mode");
-      } else {
-        console.log("Exited fullscreen mode");
-      }
+    handleFullscreenChange() {
+      this.fullscreen = fscreen.fullscreenElement !== null;
+    },
+    handleFullscreenError(err) {
+      console.error("Fullscreen error", err);
     }
   },
   beforeDestroy() {
-    fscreen.removeEventListener("fullscreenchange", this.fullscreenHandler);
+    fscreen.removeEventListener(
+      "fullscreenchange",
+      this.handleFullscreenChange
+    );
+    fscreen.removeEventListener("fullscreenerror", this.handleFullscreenError);
   }
 };
 </script>
