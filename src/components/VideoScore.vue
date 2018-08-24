d88419a9b3e0050b2f7759b40a884fd9799bed67
ashlinaronin <ashlinaronin@gmail.com>
2018-08-24 14:34:26 -0500

    Center video on page (for fullscreen)

--- src/components/VideoScore.vue
+++ src/components/VideoScore.vue
@@ -48,22 +48,14 @@ export default {
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
 video {
   display: none;
+}
+
+.video-score {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
 </style>
