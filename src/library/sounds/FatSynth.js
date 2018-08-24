5c169c3a84a25216209ec6823d9ae0c4ff409f39
ashlinaronin <ashlinaronin@gmail.com>
2018-08-24 11:46:59 -0500

    Add delay to FatSynth

--- src/library/sounds/FatSynth.js
+++ src/library/sounds/FatSynth.js
@@ -5,18 +5,24 @@ export default class FatSynth {
   constructor() {
     this.osc = new Tone.FatOscillator(80, "sawtooth");
     this.env = new Tone.AmplitudeEnvelope();
+    this.delay = new Tone.PingPongDelay(0.1, 0.1);
     this.initialize();
   }
 
   async initialize() {
     this.osc.start();
     this.osc.connect(this.env);
-    this.env.toMaster();
+    this.env.connect(this.delay);
+    this.delay.toMaster();
   }
 
   dispose() {
     this.osc.dispose();
     this.env.dispose();
+    this.delay.dispose();
+    this.osc = null;
+    this.env = null;
+    this.delay = null;
   }
 
   startNote() {
@@ -29,6 +35,8 @@ export default class FatSynth {
 
   changeParam(x, y, width, height) {
     this.osc.spread = mapRange(x, 0, width, 0, 20);
-    this.osc.detune.value = mapRange(y, 0, height, 50.0, -50.0);
+
+    const detune = mapRange(y, 0, height, 75.0, -75.0);
+    this.osc.detune.rampTo(detune);
   }
 }
