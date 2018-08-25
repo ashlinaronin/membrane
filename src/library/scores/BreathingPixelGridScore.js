912258a40b143cc4f8df2ed5dee08af6a696bc5a
ashlinaronin <ashlinaronin@gmail.com>
2018-08-24 21:42:50 -0500

    Use circles for breathing score

--- src/library/scores/BreathingPixelGridScore.js
+++ src/library/scores/BreathingPixelGridScore.js
@@ -21,16 +21,26 @@ export default class BreathingPixelGridScore extends PixelGridScore {
     this.phase = (this.phase + 0.01) % (2 * Math.PI);
     this.breath = Math.sin(this.phase) * 30;
 
-    ctx.fillStyle = gridFillStyle;
     for (let i = 0; i < this.grid.length; i++) {
       for (let j = 0; j < this.grid[0].length; j++) {
         if (this.grid[i][j] === true) {
-          ctx.fillRect(
-            this.widthUnit * i,
-            this.heightUnit * j,
-            this.widthUnit + this.breath,
-            this.heightUnit + this.breath
+          ctx.fillStyle = gridFillStyle;
+          ctx.beginPath();
+
+          this.smallerUnit =
+            this.widthUnit < this.heightUnit ? this.widthUnit : this.heightUnit;
+          this.halfSmallerUnit = this.smallerUnit / 2;
+          this.x = this.widthUnit * i + this.halfSmallerUnit;
+          this.y = this.heightUnit * j + this.halfSmallerUnit;
+
+          ctx.arc(
+            this.x,
+            this.y,
+            this.halfSmallerUnit + this.breath,
+            0,
+            2 * Math.PI
           );
+          ctx.fill();
         }
       }
     }
