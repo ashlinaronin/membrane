b0b26ca6a099e1553ada8f8aa7f3d2847333192a
ashlinaronin <ashlinaronin@gmail.com>
2019-06-02 00:27:21 -0700

    Use Times New Roman explicitly :), remove unused styles

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,8 @@ body {
 }
 
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Times New Roman", Helvetica, sans-serif;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
