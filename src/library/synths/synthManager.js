20b892c10489e5964f3ab5bdd1a9c7ac11a1423f
ashlinaronin <ashlinaronin@gmail.com>
2019-06-24 21:15:27 -0700

    Don't re-create the same synth when switching in synthManager. Fixes memory and volume issues

--- src/library/synths/synthManager.js
+++ src/library/synths/synthManager.js
@@ -3,6 +3,12 @@ import store from "../../store";
 let synth;
 
 export function changeSynthByClass(SynthClassToCreate) {
+  if (synth instanceof SynthClassToCreate) {
+    // don't re-create the same synth... can cause memory issues and was
+    // also causing a strange volume issue in RockScrapeSampler
+    return;
+  }
+
   if (typeof synth !== "undefined") {
     synth.dispose();
   }
